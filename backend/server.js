168883cd0f7f8629425b190a01403d1b87d9050e
llcain <llcain2012@gmail.com>
2023-10-11 14:36:41 -0500

    Created .gitignore file

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,11 @@ const  myBookRoues = require('./routes/myBooks');
 // express app
 const app = express();
 
+// require mongoose
+const mongoose = require('mongoose');
+
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res,next) => {
     console.log(req.path, req.method)
@@ -19,8 +22,17 @@ app.use((req, res,next) => {
 // routes
 app.use('/api/myBooks', myBookRoues)
 
-// listen for request
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for request
+        app.listen(process.env.PORT, () => {
+         console.log('connected to db & listening on port', process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
