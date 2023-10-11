8091ac0a80d4f43ae5a4b66ea6209044d9b9f7ab
llcain <llcain2012@gmail.com>
2023-10-11 12:15:30 -0500

    Created package.json file - install nodemon globally

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+// require express
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for request
+app.listen(4000, () => {
+    console.log('listening on port 4000!!');
+});
