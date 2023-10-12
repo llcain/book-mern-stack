36a2ba6f3589b2039113924ba935d1270517ad52
llcain <llcain2012@gmail.com>
2023-10-12 14:21:43 -0500

    Created my books context folder and file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MyBooksContextPrivider } from './context/MyBooksContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyBooksContextPrivider>
+      <App />
+    </MyBooksContextPrivider>    
   </React.StrictMode>
 );
 
